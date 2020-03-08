d004f68d86bb6817e4103fbdf2f6b113d5437a61
Zdravko Georgiev <32423971+r00tmebaby@users.noreply.github.com>
2020-03-08 12:51:30 +0000

    Lynda Courses Dumper
    Sould you need a complete lynda/linkedin courses list to choose a particular course from, use that small script to have the list up to date.
    
    The script create a text file with all available courses after all categories and possible pages are checked. If you interupt the program while it is still running a file won't be created since it's was still in the memory which is vlolatile.

---
+++ Lynda Courses Dump/dumplist.py
@@ -0,0 +1,32 @@
+import requests as r, re
+
+newUrlList = []
+startCategory = 1
+page = 1
+showMessage = True
+maxCategory = 2000
+
+def my_function(x):
+  return list(dict.fromkeys(x))
+
+while startCategory <= maxCategory:
+    details = r.get(url=("https://www.lynda.com/ajax/category/%s/courses?page=%s" % (startCategory, page)))
+    allUrls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', details.text)
+    if showMessage:
+        print("[*] Scraping courses from https://www.lynda.com/ajax/category/%s/courses?page=%s" % (startCategory, page))
+        print("  - %s courses are added " % len(allUrls))
+    if len(allUrls) > 2:
+        for links in allUrls:
+            if links[-2:] == 'l\\':
+                newUrlList.append(links[:-1])
+        page +=1
+    else:
+        startCategory +=1
+        page = 1
+    if startCategory == maxCategory:
+        mylist = my_function(newUrlList)
+        mylist.sort()
+        for allcourses in mylist:
+                open("LyndaCourseList.txt", "a+").write(allcourses + "\n")
+
+
